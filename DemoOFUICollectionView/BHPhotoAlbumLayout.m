4ed1bbe7041a8bdd87447a4e808e5d276ea71596
morisunshine <allenwenzhou@gmail.com>
2013-10-24 21:59:49 +0800

    fix(layout):override prepareLayout

--- DemoOFUICollectionView/BHPhotoAlbumLayout.m
+++ DemoOFUICollectionView/BHPhotoAlbumLayout.m
@@ -8,6 +8,14 @@
 
 #import "BHPhotoAlbumLayout.h"
 
+static NSString *const BHPhotoAlbumLayoutPhotoCellKind = @"PhotoCell";
+
+@interface BHPhotoAlbumLayout ()
+
+@property (nonatomic, strong) NSDictionary *layoutInfo;
+
+@end
+
 @implementation BHPhotoAlbumLayout
 
 #pragma mark - Lifecycle -
@@ -38,6 +46,55 @@
     self.itemSize = CGSizeMake(125, 125);
     self.interItemSpacingY = 12;
     self.numberOfColumns = 2;
+}
+
+#pragma mark - Layout - 
+
+- (void)prepareLayout
+{
+    NSMutableDictionary *newLayoutInfo = [NSMutableDictionary dictionary];
+    NSMutableDictionary *cellLayoutInfo = [NSMutableDictionary dictionary];
+    
+    NSInteger sectionCount = [self.collectionView numberOfSections];
+    NSIndexPath *indexPath = [NSIndexPath indexPathForItem:0 inSection:0];
+    
+    for (NSInteger section = 0; section < sectionCount; section++) {
+        NSInteger itemCount = [self.collectionView numberOfItemsInSection:section];
+        
+        for (NSInteger item = 0; item < itemCount; item++) {
+            indexPath = [NSIndexPath indexPathForItem:item inSection:section];
+            
+            UICollectionViewLayoutAttributes *itemAttributes = [UICollectionViewLayoutAttributes layoutAttributesForCellWithIndexPath:indexPath];
+            itemAttributes.frame = [self frameForAlbumPhotoAtIndexPath:indexPath];
+            cellLayoutInfo[indexPath] = itemAttributes;
+        }
+    }
+    
+    newLayoutInfo[BHPhotoAlbumLayoutPhotoCellKind] = cellLayoutInfo;
+    
+    self.layoutInfo = newLayoutInfo;
+}
+
+
+#pragma mark - Private
+
+- (CGRect)frameForAlbumPhotoAtIndexPath:(NSIndexPath *)indexPath
+{
+    NSInteger row = indexPath.section / self.numberOfColumns;
+    NSInteger column = indexPath.section % self.numberOfColumns;
+    
+    CGFloat spacingX = self.collectionView.bounds.size.width -
+                       self.itemInsets.left -
+                       self.itemInsets.right -
+                       (self.numberOfColumns * self.itemSize.width);
+    if (self.numberOfColumns > 1) {
+        spacingX = spacingX / (self.numberOfColumns - 1);
+    }
+    
+    CGFloat originX = floorf(self.itemInsets.left + (self.itemSize.width + spacingX) * column);
+    CGFloat originY = floorf(self.itemInsets.top + (self.itemSize.height + self.interItemSpacingY) * row);
+    
+    return CGRectMake(originX, originY, self.itemSize.width, self.itemSize.height);
 }
 
 @end
