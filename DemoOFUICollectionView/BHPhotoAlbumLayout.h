1f6c7dea261208882bdd949bdc3f024209859925
morisunshine <allenwenzhou@gmail.com>
2013-10-24 21:21:46 +0800

    feat:create a UICollectionViewLayout named BHPhotoAlbumLayout

---
+++ DemoOFUICollectionView/BHPhotoAlbumLayout.h
@@ -0,0 +1,13 @@
+//
+//  BHPhotoAlbumLayout.h
+//  DemoOFUICollectionView
+//
+//  Created by Sheldon on 13-10-24.
+//  Copyright (c) 2013年 Sheldon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BHPhotoAlbumLayout : UICollectionViewLayout
+
+@end
---
+++ DemoOFUICollectionView/BHPhotoAlbumLayout.m
@@ -0,0 +1,13 @@
+//
+//  BHPhotoAlbumLayout.m
+//  DemoOFUICollectionView
+//
+//  Created by Sheldon on 13-10-24.
+//  Copyright (c) 2013年 Sheldon. All rights reserved.
+//
+
+#import "BHPhotoAlbumLayout.h"
+
+@implementation BHPhotoAlbumLayout
+
+@end
